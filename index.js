b834df4bdaffb465df2889d394979312540a2f3d
Marharyta Bosak <aboimova.rita@gmail.com>
2021-11-15 17:02:42 +0200

    mv functions to class

--- index.js
+++ index.js
@@ -24,6 +24,24 @@ class CountdownTimer {
       return;
     }
 
+    function pad(value) {
+      return String(value).padStart(2, '0');
+    }
+
+    function getTimeComponents(time) {
+      const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+
+      const hours = pad(
+        Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+      );
+
+      const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+
+      const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+
+      return { days, hours, mins, secs };
+    }
+
     this.intervalId = setInterval(() => {
       if (this.targetDate <= currentDate) {
         return;
@@ -54,22 +72,4 @@ function updateClockComponents({ days, hours, mins, secs }) {
   refs.hours.textContent = `${hours}`;
   refs.mins.textContent = `${mins}`;
   refs.secs.textContent = `${secs}`;
-}
-
-function pad(value) {
-  return String(value).padStart(2, '0');
-}
-
-function getTimeComponents(time) {
-  const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-
-  const hours = pad(
-    Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-  );
-
-  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-
-  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
-
-  return { days, hours, mins, secs };
 }
